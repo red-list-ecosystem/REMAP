e1e60c56dad76dc1927af5f24a30cb28144a91c8
johnWilshire <john.h.wilshire@gmail.com>
2018-06-29 14:25:41 +1000

    updated vunerable packages

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ from google.appengine.api import urlfetch
 WSGI_KEY = open('secrets/wsgi.txt').read().strip()
 EE_ACCOUNT = open('secrets/ee_account.txt').read().strip()
 EE_PRIVATE_KEY_FILE = 'secrets/gee_service_account_secrets.json'
-OAUTH_FILE = 'secrets/client_secrets.json'
+OAUTH_FILE = 'secrets/oauth_secret.json'
 
 EE_CREDENTIALS = ee.ServiceAccountCredentials(EE_ACCOUNT, EE_PRIVATE_KEY_FILE)
 ee.Initialize(EE_CREDENTIALS)
@@ -20,7 +20,9 @@ MAX_POINTS = 2500
 # area threshold in km^, the area of the region cannot exceed this
 AREA_THRESHOLD = 100000
 
-CITATION = 'Murray, N.J., Keith, D.A., Simpson, D., Wilshire, J.H. & Lucas, R.M. (2017) REMAP: An online remote sensing application for land cover classification and monitoring. bioRxiv. DOI: 10.1101/212464'
+CITATION = 'Murray, N. J., Keith, D. A., Simpson, D. , Wilshire, J. H. and Lucas, R. M. (2018), Remap: An online remote sensing application for land cover classification and monitoring. Methods Ecol Evol. Accepted Author Manuscript. . doi:10.1111/2041-210X.13043'
+# old citation:
+# CITATION = 'Murray, N.J., Keith, D.A., Simpson, D., Wilshire, J.H. & Lucas, R.M. (2017) REMAP: An online remote sensing application for land cover classification and monitoring. bioRxiv. DOI: 10.1101/212464'
 # flag to check if we are running in debug mode or not
 DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
 
@@ -41,7 +43,7 @@ SAMPLE_DATA = [
     'json': 'remap_training_chedubaMyanmar.json',
     'csv': 'remap_points_chedubaMyanmar.csv'
   },
-  { 
+  {
     'name': 'Dubai Coastal Reclamation Dataset (2003)',
     'json': 'remap_training_Dubai_2003.json',
     'csv': 'remap_points_Dubai_2003.csv'
